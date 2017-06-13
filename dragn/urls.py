645c2b7179ff7950f09c4b5830f87ab7531bf47f
Madjura <madjura@hotmail.com>
2017-06-13 13:45:35 +0200

    completely renamed

--- dragn/urls.py
+++ dragn/urls.py
@@ -1,4 +1,4 @@
-"""skimmr_django URL Configuration
+"""dragn URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- dragn/wsgi.py
+++ dragn/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for skimmr_django project.
+WSGI config for dragn project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
