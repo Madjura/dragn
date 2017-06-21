797a438dfa5cf2b3f7b8688959df14f8e78a06a9
Madjura <madjura@hotmail.com>
2017-06-21 14:07:21 +0200

    added copyright / docstring where requried

--- query/queryresult.py
+++ query/queryresult.py
@@ -1,3 +1,28 @@
+"""
+Copyright (C) 2012 Vit Novacek (vit.novacek@deri.org), Digital Enterprise
+Research Institute (DERI), National University of Ireland Galway (NUIG)
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This class is based on the MemStoreQueryResult class of the original system.
+The following functions were used from the original system:
+    - populate_dictionaries
+
+    Those functions were used as a base to develop the new ones in this file.
+"""
+
 import gzip
 import os
 from _collections import defaultdict
